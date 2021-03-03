122be28264dc1c52070f8ab2a773bcb91e328610
Christopher Stock <christopher.stock@mayflower.de>
2021-03-03 09:47:52 +0100

    Add dependency jackson, route json and response.

--- src/Application.kt
+++ src/Application.kt
@@ -1,7 +1,10 @@
 package de.mayflower
 
+import com.fasterxml.jackson.databind.SerializationFeature
 import io.ktor.application.*
+import io.ktor.features.*
 import io.ktor.http.*
+import io.ktor.jackson.*
 import io.ktor.response.*
 import io.ktor.routing.*
 
@@ -10,9 +13,30 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
     routing {
         get("/") {
             call.respondText("Hello Backend!", contentType = ContentType.Text.Plain)
+        }
+        get("/json") {
+            val status = "success"
+            val id = 1
+            val text = "Example JSON data string"
+
+            call.respond(
+                mapOf(
+                    "status" to status,
+                    "value" to mapOf(
+                        "id" to id,
+                        "text" to text
+                    )
+                )
+            )
         }
     }
 }
